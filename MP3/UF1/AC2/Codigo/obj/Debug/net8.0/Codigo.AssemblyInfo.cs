47cb1a2a8a6f352029d4084e2b7afaf57d0e2419
solerdidac <146431787+solerdidac@users.noreply.github.com>
2024-10-08 17:45:39 +0200

    Actividad

--- MP3/UF1/AC2/Codigo/obj/Debug/net8.0/Codigo.AssemblyInfo.cs
+++ MP3/UF1/AC2/Codigo/obj/Debug/net8.0/Codigo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codigo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6e00a8e781513efd0ae70270572e725ec2679b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7bad580cad793c166280273dc7f789425c717c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codigo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codigo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
